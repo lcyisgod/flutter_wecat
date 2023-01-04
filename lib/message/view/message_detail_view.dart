a28d8bf396a396ceddc6f4933c29fba72330e438
liangchengyou <2503978335@qq.com>
2023-01-04 16:16:47 +0800

    fit:消息详情页面

--- lib/message/view/message_detail_view.dart
+++ lib/message/view/message_detail_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wecat/common/res/colors.dart';
+import 'package:flutter_wecat/message/widget/message_detail_info_item.dart';
+import 'package:oktoast/oktoast.dart';
 
 class MessageDetailView extends StatefulWidget {
   @override
@@ -10,6 +12,25 @@ class MessageDetailView extends StatefulWidget {
 }
 
 class MessageDetailViewState extends State<MessageDetailView> {
+  List<Map> dataList = [];
+
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    for(int i = 0; i < 10; i++) {
+      Map messageData = {
+        "imageUrl" : i % 3 == 0 ? "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2F9f7069ccbb537742fa8946d4dd3b9624fd9025df.jpg&refer=http%3A%2F%2Fi1.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1672973706&t=d20919bee00b32dc72f6a5b4047698bf" : "https://nimg.ws.126.net/?url=http%3A%2F%2Fdingyue.ws.126.net%2F2022%2F0820%2Fa3fabb7aj00rgw36200a6d000hs00k5p.jpg&thumbnail=660x2147483647&quality=80&type=jpg",
+        "message" : "这是第"+i.toString()+"条消息",
+        "userType" : i % 3 == 0 ? "0" : "1"
+      };
+      dataList.add(messageData);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -20,14 +41,11 @@ class MessageDetailViewState extends State<MessageDetailView> {
           Expanded(
               child: Container(
                 child: ListView.builder(
-                    itemCount: 100,
+                    itemCount: dataList.length,
                     shrinkWrap:true,
                     itemBuilder: (BuildContext context, int index){
-                      return Container(
-                        height: 50,
-                        child: Text(
-                            "文字信息"
-                        ),
+                      return MessageDetailInfoItem(
+                        messageData: dataList[index],
                       );
                     }),
               )
@@ -59,6 +77,22 @@ class MessageDetailViewState extends State<MessageDetailView> {
                             isCollapsed:true,
                             border: InputBorder.none,
                           ),
+                          controller: controller,
+                          onSubmitted: (String text) {
+                            if(text.isEmpty) {
+                              showToast("消息不能为空");
+                              return;
+                            }
+                            setState(() {
+                              Map dataMap = {
+                                "imageUrl" : "https://nimg.ws.126.net/?url=http%3A%2F%2Fdingyue.ws.126.net%2F2022%2F0820%2Fa3fabb7aj00rgw36200a6d000hs00k5p.jpg&thumbnail=660x2147483647&quality=80&type=jpg",
+                                "message" : text,
+                                "userType" : "1"
+                              };
+                              dataList.add(dataMap);
+                              controller.text = "";
+                            });
+                          },
                         ),
                       )
                   ),
---
+++ lib/message/widget/message_detail_info_item.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wecat/common/res/colors.dart';
+
+class MessageDetailInfoItem extends StatelessWidget {
+
+  ///聊天消息
+  final Map messageData;
+
+  const MessageDetailInfoItem({Key key, this.messageData}) : super(key: key);
+
+
+  Widget createInfoItemWithData(BuildContext context,Map messageData) {
+    if(messageData["userType"] == "1") {///当前消息发送人为主人
+      return Container(
+        padding: EdgeInsets.only(top: 2.0,bottom: 2.0,left: 5.0,right: 5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(right: 5.0),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        minHeight: 30,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.all(Radius.circular(3))
+                    ),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(left: 2.0,right: 2.0),
+                    child: Text(
+                      messageData["message"],
+                      maxLines : 9999,
+                      style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.black
+                      ),
+                    ),
+                  ),
+                )
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(3.0),
+              child: Image.network(messageData["imageUrl"],fit: BoxFit.cover,height: 30, width: 30,),
+            )
+          ],
+        ),
+      );
+    }
+    return Container(
+      padding: EdgeInsets.only(top: 2.0,bottom: 2.0,left: 5.0,right: 5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(3.0),
+            child: Image.network(messageData["imageUrl"],fit: BoxFit.cover,height: 30, width: 30,),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.centerLeft,
+              constraints: BoxConstraints(
+                  minHeight: 30
+              ),
+              decoration: BoxDecoration(
+                  color: Colours.bg_color,
+                  borderRadius: BorderRadius.all(Radius.circular(3))
+              ),
+              margin: EdgeInsets.only(left: 5.0),
+              padding: EdgeInsets.only(left: 2.0,right: 2.0),
+              child: Text(
+                messageData["message"],
+                maxLines : 9999,
+                style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.black
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return createInfoItemWithData(context, messageData);
+  }
+}
